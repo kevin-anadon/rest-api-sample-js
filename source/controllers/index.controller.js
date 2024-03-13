37b8ebf4acdbd1ba548bdca36aac77f81b90ce1a
Kevin Anadon <mathewanadon@gmail.com>
2024-03-13 12:15:17 -0300

    test for devops pipelines (ibm cloud toolchains)

--- source/controllers/index.controller.js
+++ source/controllers/index.controller.js
@@ -2,7 +2,7 @@ const { request, response } = require("express");
 
 const get_index = async (req = request, res = response) => {
     try {
-        let msg = "Welcome to the index page! \nThis is an API Sample JS app.";
+        let msg = "<h1>Welcome to the index page!!</h1><br><h2>this is an API Sample JS app.</h2><br><p>Example paragraph.</p>";
 
         res.send(msg);  
     } catch (error) {
