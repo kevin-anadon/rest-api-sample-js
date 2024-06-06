11ad7a10d777236243a40e4db1636d20d7b1cdfa
Kevin Anadon <mathewanadon@gmail.com>
2024-06-06 10:34:15 -0300

    toolchain test

--- source/controllers/index.controller.js
+++ source/controllers/index.controller.js
@@ -2,7 +2,7 @@ const { request, response } = require("express");
 
 const get_index = async (req = request, res = response) => {
     try {
-        let msg = "<h1>Welcome to the index page!!</h1><br><h2>this is an API Sample JS app.</h2><br><p>Example paragraph.</p>";
+        let msg = "<h1>Welcome to the index page!!</h1><br><h2>this is an API Sample JS app.</h2><br><p>Example paragraph. DevOps Test</p>";
 
         res.send(msg);  
     } catch (error) {
